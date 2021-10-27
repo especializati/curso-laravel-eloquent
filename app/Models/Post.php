987b42864d849979869aaec66924cfd34412dc7d
Carlos Ferreira <conta@especializati.com.br>
2021-10-27 15:46:58 -0300

    Eloquent Model Options

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    // protected $table = 'postagens';
+    // protected $primaryKey = 'id_postagem';
+    // protected $keyType = 'string';
+    // protected $incrementing = false;
+    // protected $timestamps = true;
+    // const CREATED_AT = 'data_criacao';
+    // const UPDATED_AT = 'data_atualizacao';
+    // protected $dateFormat = 'd/m/Y';
+    // protected $connection = 'pgsql';
+    // protected $attributes = [
+    //     'active' => true
+    // ];
 }

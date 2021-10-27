8e31dfae5ce2e45ed5654d1a633961fed2bc6162
Carlos Ferreira <conta@especializati.com.br>
2021-10-27 16:08:37 -0300

    Example Selects with Eloquent

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/select', function () {
+    // $users = User::all();
+    // $users = User::where('id', 1)->get();
+    // $user = User::where('id', 10)->first();
+    // $user = User::first();
+    // $user = User::find(101);
+    // $user = User::findOrFail(request('id'));
+    // $user = User::where('name', request('name'))->firstOrFail();
+    // $user = User::firstWhere('name', request('name'));
+
+    // dd($user);
+});
 
 Route::get('/', function () {
     return view('welcome');

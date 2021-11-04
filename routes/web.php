6052f0a76e584cdd97f3ce22e5ec15444db60cd6
Carlos Ferreira <conta@especializati.com.br>
2021-11-04 15:31:28 -0300

    Options where Laravel

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,25 @@
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/where', function (User $user) {
+    $filter = 'a';
+
+    // $user = $user->where('email', '=', 'ikunde@example.com')->first();
+    // $user = $user->where('email', 'ikunde@example.com')->first();
+    // // $users = $user->where('name', 'LIKE', "%{$filter}%")->get();
+    // $users = $user->where('name', 'LIKE', "%{$filter}%")
+    //                 ->whereIn('email', [])
+    //                 ->get();
+    $users = $user->where('name', 'LIKE', "%{$filter}%")
+                    ->orWhere(function ($query) use ($filter) {
+                        $query->where('name', '<>', 'Carlos');
+                        $query->where('name', '=', $filter);
+                    })
+                    ->toSql();
+
+    dd($users);
+});
+
 Route::get('/select', function () {
     // $users = User::all();
     // $users = User::where('id', 1)->get();
